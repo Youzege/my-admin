6db1d2f1cfc8a5e06e988b30f688f30876f1b5a3
bear-eat-bear-biscuit <153716101@qq.com>
2021-11-28 21:10:28 +0800

    feat: request 响应拦截器处理数据

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,8 +1,29 @@
 import axios from 'axios'
+import { ElMessage } from 'element-plus'
 
 const service = axios.create({
   baseURL: process.env.VUE_APP_BASE_API,
   timeout: 5000
 })
+
+// 响应拦截器
+service.interceptors.response.use(
+  (response) => {
+    const { success, message, data } = response.data
+    // 根据success决定下面操作
+    if (success) {
+      return data
+    } else {
+      // 业务错误
+      ElMessage.error(message)
+      return Promise.reject(new Error(message))
+    }
+  },
+  (error) => {
+    // TODO: 处理token超时
+    ElMessage.error(error.message)
+    return Promise.reject(error)
+  }
+)
 
 export default service
--- src/utils/storage.js
+++ src/utils/storage.js
@@ -20,3 +20,17 @@ export const getItem = (key) => {
     return data
   }
 }
+
+/**
+ * 删除指定数据
+ */
+export const removeItem = (key) => {
+  window.localStorage.removeItem(key)
+}
+
+/**
+ * 删除所有数据
+ */
+export const removeAllItem = () => {
+  window.localStorage.clear()
+}
