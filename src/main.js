0fd15924b3cedfe1751984e674d621d2c57b79aa
bear-eat-bear-biscuit <153716101@qq.com>
2021-11-28 14:31:41 +0800

    feat(all): add element-plus

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import installElementPlus from './plugins/element'
 import router from './router'
 import store from './store'
 
 const app = createApp(App)
-
+installElementPlus(app)
 app.use(store).use(router).mount('#app')
