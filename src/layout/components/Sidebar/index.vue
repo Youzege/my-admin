11f560b0061e80fcb2fa8101bf121ac1f6e567f3
bear-eat-bear-biscuit <153716101@qq.com>
2021-11-29 13:43:58 +0800

    feat(views/router): 添加views页面、router私有路由表

--- src/layout/components/Sidebar/index.vue
+++ src/layout/components/Sidebar/index.vue
@@ -1,8 +1,14 @@
 <template>
-	<div>Sidebar</div>
+	<div>
+		<h1>hello</h1>
+		<el-scrollbar>
+			<sidebar-menu></sidebar-menu>
+		</el-scrollbar>
+	</div>
 </template>
 
 <script setup>
+	import SidebarMenu from './SidebarMenu.vue'
 	import {} from 'vue'
 </script>
 
