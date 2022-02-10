112a0cbb30c9fe7d685ed06d4d9c2f1568c86f68
Youzege <youzegehq@gmail.com>
2022-02-10 21:59:14 +0800

    feat: themeSelect组件

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -1,101 +1,103 @@
 <template>
-	<div class="navbar">
-		<hamburger class="hamburger-container" />
-		<breadcrumb class="breadcrumb-container" />
-		<div class="right-menu">
-			<lang-select class="right-menu-item" />
-			<!-- 头像 -->
-			<el-dropdown class="avatar-container" trigger="click">
-				<div class="avatar-wrapper">
-					<el-avatar
-						shape="square"
-						:size="40"
-						:src="'http://nimg.ws.126.net/?url=http://dingyue.ws.126.net/2021/0410/e0c0cd5cj00qrbv4e001md200jg00jgg00960096.jpg&thumbnail=650x2147483647&quality=80&type=jpg'"
-					></el-avatar>
-					<i class="el-icon-s-tools"></i>
-				</div>
-				<template #dropdown>
-					<el-dropdown-menu class="user-dropdown">
-						<router-link to="/">
-							<el-dropdown-item> {{ $t('msg.navBar.home') }} </el-dropdown-item>
-						</router-link>
-						<a target="_blank" href="https://juejin.cn/user/4160992475616408">
-							<el-dropdown-item>{{ $t('msg.navBar.course') }}</el-dropdown-item>
-						</a>
-						<el-dropdown-item divided @click="logout">
-							{{ $t('msg.navBar.logout') }}
-						</el-dropdown-item>
-					</el-dropdown-menu>
-				</template>
-			</el-dropdown>
-		</div>
-	</div>
+  <div class="navbar">
+    <hamburger class="hamburger-container" />
+    <breadcrumb class="breadcrumb-container" />
+    <div class="right-menu">
+      <theme-select class="right-menu-item hover-effect" />
+      <lang-select class="right-menu-item" />
+      <!-- 头像 -->
+      <el-dropdown class="avatar-container" trigger="click">
+        <div class="avatar-wrapper">
+          <el-avatar
+            shape="square"
+            :size="40"
+            :src="'http://nimg.ws.126.net/?url=http://dingyue.ws.126.net/2021/0410/e0c0cd5cj00qrbv4e001md200jg00jgg00960096.jpg&thumbnail=650x2147483647&quality=80&type=jpg'"
+          ></el-avatar>
+          <i class="el-icon-s-tools"></i>
+        </div>
+        <template #dropdown>
+          <el-dropdown-menu class="user-dropdown">
+            <router-link to="/">
+              <el-dropdown-item> {{ $t('msg.navBar.home') }} </el-dropdown-item>
+            </router-link>
+            <a target="_blank" href="https://juejin.cn/user/4160992475616408">
+              <el-dropdown-item>{{ $t('msg.navBar.course') }}</el-dropdown-item>
+            </a>
+            <el-dropdown-item divided @click="logout">
+              {{ $t('msg.navBar.logout') }}
+            </el-dropdown-item>
+          </el-dropdown-menu>
+        </template>
+      </el-dropdown>
+    </div>
+  </div>
 </template>
 
 <script setup>
-	import Breadcrumb from '@/components/Breadcrumb/index.vue'
-	import Hamburger from '@/components/Hamburger'
-	import LangSelect from '@/components/LangSelect/index.vue'
-	import { useStore } from 'vuex'
+import Breadcrumb from '@/components/Breadcrumb/index.vue'
+import Hamburger from '@/components/Hamburger'
+import LangSelect from '@/components/LangSelect/index.vue'
+import ThemeSelect from '@/components/ThemeSelect/index.vue'
+import { useStore } from 'vuex'
 
-	const store = useStore()
-	const logout = () => {
-		store.dispatch('user/logout')
-	}
+const store = useStore()
+const logout = () => {
+  store.dispatch('user/logout')
+}
 </script>
 
-<style lang='scss' scoped>
-	.navbar {
-		height: 50px;
-		overflow: hidden;
-		position: relative;
-		background: #fff;
-		box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
-		.hamburger-container {
-			float: left;
-			height: 100%;
-			line-height: 46px;
-			cursor: pointer;
+<style lang="scss" scoped>
+.navbar {
+  height: 50px;
+  overflow: hidden;
+  position: relative;
+  background: #fff;
+  box-shadow: 0 1px 4px rgba(0, 21, 41, 0.08);
+  .hamburger-container {
+    float: left;
+    height: 100%;
+    line-height: 46px;
+    cursor: pointer;
 
-			// hover 动画
-			transition: background 0.5s;
-			&:hover {
-				background: rgba(0, 0, 0, 0.1);
-			}
-		}
-		.breadcrumb-container {
-			float: left;
-		}
+    // hover 动画
+    transition: background 0.5s;
+    &:hover {
+      background: rgba(0, 0, 0, 0.1);
+    }
+  }
+  .breadcrumb-container {
+    float: left;
+  }
 
-		.right-menu {
-			display: flex;
-			align-items: center;
-			float: right;
-			padding-right: 16px;
+  .right-menu {
+    display: flex;
+    align-items: center;
+    float: right;
+    padding-right: 16px;
 
-			::v-deep .right-menu-item {
-				display: inline-block;
-				padding: 0 18px 0 0;
-				font-size: 24px;
-				color: #5a5e66;
-				vertical-align: text-bottom;
+    ::v-deep .right-menu-item {
+      display: inline-block;
+      padding: 0 18px 0 0;
+      font-size: 24px;
+      color: #5a5e66;
+      vertical-align: text-bottom;
 
-				&.hover-effect {
-					cursor: pointer;
-				}
-			}
+      &.hover-effect {
+        cursor: pointer;
+      }
+    }
 
-			::v-deep .avatar-container {
-				cursor: pointer;
-				.avatar-wrapper {
-					margin-top: 5px;
-					position: relative;
-					.el-avatar {
-						--el-avatar-background-color: none;
-						margin-right: 12px;
-					}
-				}
-			}
-		}
-	}
+    ::v-deep .avatar-container {
+      cursor: pointer;
+      .avatar-wrapper {
+        margin-top: 5px;
+        position: relative;
+        .el-avatar {
+          --el-avatar-background-color: none;
+          margin-right: 12px;
+        }
+      }
+    }
+  }
+}
 </style>
