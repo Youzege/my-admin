aa79565a75968b997c1e3282aa9ff869ae38a7cc
Youzege <youzgehq@gmail.com>
2022-02-10 20:29:06 +0800

    feat: siderbar/breadcrumb 国际化

--- src/components/Breadcrumb/index.vue
+++ src/components/Breadcrumb/index.vue
@@ -1,81 +1,82 @@
 <template>
-	<el-breadcrumb class="breadcrumb" separator="/">
-		<transition-group name="breadcrumb">
-			<el-breadcrumb-item
-				v-for="(item, index) in breadcrumbData"
-				:key="item.path"
-			>
-				<!-- 不可点击面包屑 -->
-				<span class="no-redirect" v-if="index === breadcrumbData.length - 1">{{
-					item.meta.title
-				}}</span>
-				<!-- 可点击面包屑 -->
-				<a
-					v-else
-					class="redirect"
-					:size="linkHoverColor"
-					@click.prevent="onLinkClick(item)"
-				>
-					{{ item.meta.title }}
-				</a>
-			</el-breadcrumb-item>
-		</transition-group>
-	</el-breadcrumb>
+  <el-breadcrumb class="breadcrumb" separator="/">
+    <transition-group name="breadcrumb">
+      <el-breadcrumb-item
+        v-for="(item, index) in breadcrumbData"
+        :key="item.path"
+      >
+        <!-- 不可点击面包屑 -->
+        <span class="no-redirect" v-if="index === breadcrumbData.length - 1">{{
+          generateTitle(item.meta.title)
+        }}</span>
+        <!-- 可点击面包屑 -->
+        <a
+          v-else
+          class="redirect"
+          :size="linkHoverColor"
+          @click.prevent="onLinkClick(item)"
+        >
+          {{ generateTitle(item.meta.title) }}
+        </a>
+      </el-breadcrumb-item>
+    </transition-group>
+  </el-breadcrumb>
 </template>
 
 <script setup>
-	import { ref, watch } from 'vue'
-	import { useRoute, useRouter } from 'vue-router'
-	import { useStore } from 'vuex'
+import { ref, watch } from 'vue'
+import { useRoute, useRouter } from 'vue-router'
+import { useStore } from 'vuex'
+import { generateTitle } from '@/utils/i18n'
 
-	// 处理点击事件
-	const router = useRouter()
-	const onLinkClick = (item) => {
-		router.push(item.path)
-	}
+// 处理点击事件
+const router = useRouter()
+const onLinkClick = (item) => {
+  router.push(item.path)
+}
 
-	const route = useRoute()
-	// 生成数组数据
-	const breadcrumbData = ref([])
-	const getBreadcrumbData = () => {
-		breadcrumbData.value = route.matched.filter(
-			(item) => item.meta && item.meta.title
-		)
-	}
-	// 监听路由
-	watch(
-		route,
-		() => {
-			getBreadcrumbData()
-		},
-		{
-			immediate: true
-		}
-	)
+const route = useRoute()
+// 生成数组数据
+const breadcrumbData = ref([])
+const getBreadcrumbData = () => {
+  breadcrumbData.value = route.matched.filter(
+    (item) => item.meta && item.meta.title
+  )
+}
+// 监听路由
+watch(
+  route,
+  () => {
+    getBreadcrumbData()
+  },
+  {
+    immediate: true
+  }
+)
 
-	// 主题色替换变量
-	const store = useStore()
-	const linkHoverColor = ref(store.getters.cssVar.menuBg)
+// 主题色替换变量
+const store = useStore()
+const linkHoverColor = ref(store.getters.cssVar.menuBg)
 </script>
 
-<style lang='scss' scoped>
-	.breadcrumb {
-		display: inline-block;
-		font-size: 14px;
-		line-height: 50px;
-		margin-left: 8px;
+<style lang="scss" scoped>
+.breadcrumb {
+  display: inline-block;
+  font-size: 14px;
+  line-height: 50px;
+  margin-left: 8px;
 
-		::v-deep .no-redirect {
-			color: #97a8be;
-			cursor: text;
-		}
-		.redirect {
-			color: #666;
-			font-weight: 600;
-		}
-		.redirect:hover {
-			// 将来需要进行主题色替换
-			color: v-bind(linkHoverColor);
-		}
-	}
+  ::v-deep .no-redirect {
+    color: #97a8be;
+    cursor: text;
+  }
+  .redirect {
+    color: #666;
+    font-weight: 600;
+  }
+  .redirect:hover {
+    // 将来需要进行主题色替换
+    color: v-bind(linkHoverColor);
+  }
+}
 </style>
--- src/layout/components/Sidebar/MenuItem.vue
+++ src/layout/components/Sidebar/MenuItem.vue
@@ -1,29 +1,30 @@
 <template>
-	<!-- element icon -->
-	<i v-if="icon.includes('el-icon')" class="sub-el-icon" :class="icon"></i>
-	<!-- my svg icon -->
-	<svg-icon v-else :icon="icon"></svg-icon>
-	<span>{{ title }}</span>
+  <!-- element icon -->
+  <i v-if="icon.includes('el-icon')" class="sub-el-icon" :class="icon"></i>
+  <!-- my svg icon -->
+  <svg-icon v-else :icon="icon"></svg-icon>
+  <span>{{ generateTitle(title) }}</span>
 </template>
 
 <script setup>
-	import { defineProps } from 'vue'
-	defineProps({
-		title: {
-			type: String,
-			required: true
-		},
-		icon: {
-			type: String,
-			required: true
-		}
-	})
+import { defineProps } from 'vue'
+import { generateTitle } from '@/utils/i18n'
+defineProps({
+  title: {
+    type: String,
+    required: true
+  },
+  icon: {
+    type: String,
+    required: true
+  }
+})
 </script>
 
-<style lang='scss' scoped>
-	.sub-el-icon {
-		color: currentColor;
-		width: 1em;
-		height: 1em;
-	}
+<style lang="scss" scoped>
+.sub-el-icon {
+  color: currentColor;
+  width: 1em;
+  height: 1em;
+}
 </style>
