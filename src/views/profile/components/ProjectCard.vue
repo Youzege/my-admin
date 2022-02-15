a0536ef24c74fdd39093e14a5c84000a7c6409f3
Youzege <youzegehq@gmail.com>
2022-02-15 21:52:08 +0800

    feat: panThumb头像组件

---
+++ src/views/profile/components/ProjectCard.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <pan-thumb
+      :image="imgUrl"
+      :height="'100px'"
+      :width="'100px'"
+      :hoverable="false"
+      ><div>Hi</div>
+      {{ $store.getters.userInfo.title }}
+    </pan-thumb>
+  </div>
+</template>
+
+<script setup>
+import PanThumb from '@/components/PanThumb'
+import { ref } from 'vue'
+import cat from './../../../assets/cat.png'
+
+const imgUrl = ref(cat)
+</script>
+
+<style lang="scss" scoped></style>
