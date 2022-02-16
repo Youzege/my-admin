b2ed130c83067dae05180780bca591e1d0ab083a
Youzege <youzegehq@gmail.com>
2022-02-16 09:56:19 +0800

    feat: profile->Author/Chapter作者、时间线组件

--- src/views/profile/components/Author.vue
+++ src/views/profile/components/Author.vue
@@ -1,9 +1,50 @@
 <template>
-  <div>作者</div>
+  <div class="author-container">
+    <div class="header">
+      <pan-thumb
+        image="http://nimg.ws.126.net/?url=http://dingyue.ws.126.net/2021/0410/e0c0cd5cj00qrbv4e001md200jg00jgg00960096.jpg&thumbnail=650x2147483647&quality=80&type=jpg"
+        height="60px"
+        width="60px"
+        :hoverable="false"
+      >
+        {{ $t('msg.profile.name') }}
+      </pan-thumb>
+      <div class="header-desc">
+        <h3>{{ $t('msg.profile.name') }}</h3>
+        <span>{{ $t('msg.profile.job') }}</span>
+      </div>
+    </div>
+    <div class="info">
+      {{ $t('msg.profile.Introduction') }}
+    </div>
+  </div>
 </template>
 
 <script setup>
+import PanThumb from '@/components/PanThumb/index.vue'
 import {} from 'vue'
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.author-container {
+  .header {
+    display: flex;
+    .header-desc {
+      margin-left: 12px;
+      display: flex;
+      flex-direction: column;
+      justify-content: space-around;
+
+      span {
+        font-size: 14px;
+      }
+    }
+  }
+  .info {
+    margin-top: 16px;
+    line-height: 22px;
+    font-size: 14px;
+    text-indent: 26px;
+  }
+}
+</style>
